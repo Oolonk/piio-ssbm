d1ebce84cb5639b6d8a813d15ba1572b4560edaa
Oolonk <dortimus51@gmail.com>
2023-01-23 00:38:25 +0100

    add slippi options

--- js/main.js
+++ js/main.js
@@ -156,6 +156,9 @@ async function applyClientSettings(settings){
 				client.fixedSmashggQueue = row.value;
 				document.body.classList.toggle("fixedSmashggQueue", row.value);
 			break;
+			case "relay-port":
+				console.log(row.value);
+			break;
 		}
 	}
 }
